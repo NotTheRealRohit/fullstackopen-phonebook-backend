6d14985033cc23c762b3a8924a7f5b54151ae1ab
NotTheRealRohit <iam.rohitsaji@gmail.com>
2024-03-17 16:29:32 +0530

    Added frontendDist to backend with Validators

--- model/person.js
+++ model/person.js
@@ -12,8 +12,21 @@ mongoose.connect(url)
     })
 
 const personSchema = mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type:String,
+        minLength: 3,
+        required:true,
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        validate:{
+            validator: function(value){
+                return /^\d{2,3}-\d+$/.test(value);
+            },
+            message: props=> `${props.value} is not a valid phone number`
+        }
+    }
 })
 
 personSchema.set("toJSON",{
